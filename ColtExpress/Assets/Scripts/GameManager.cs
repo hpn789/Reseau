f2cdf3d3a2f326183aa0d3ccd7e20726cca0a799
Maxime Rignault <maximerignault@gmail.com>
2017-06-10 18:55:36 +0200

    Added scores panel

---
+++ ColtExpress/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _panel;
+
+    [SerializeField]
+    Text[] _scores;
+
+    public void EndGame(int[] scores)
+    {
+        _panel.SetActive(true);
+        for(int i = 0; i < _scores.Length; i++)
+        {
+            _scores[i].text = scores[i].ToString();
+        }
+    }
+}
